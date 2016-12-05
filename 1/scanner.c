7cac837cf0edf8134cba8a479d824ccafb9ce698
Wu Haotian <whtsky@gmail.com>
2016-12-05 16:14:39 +0800

    [WIP] exp 1

--- 1/scanner.c
+++ 1/scanner.c
@@ -1,8 +1,15 @@
-#include <stdbool.h>
+#include <stdio.h>
+
 
 enum Token {
+    T_END=-1,
+    T_ERROR=0,
     T_SYMBOL=1,
-    T_CONSTANT, T_INT, T_IF
+    T_CONSTANT, T_INTEGER, T_IF, T_THEN, T_ELSE,
+    T_BEGIN, T_END, T_READ, T_WRITE, T_FUNCTION,
+    T_ASSIGN, T_L, T_LE, T_G, T_GE,
+    T_ADD, T_SUB, T_MUL, T_DIV,
+    T_E, T_NE
 };
 
 struct symTableItem{
@@ -13,8 +20,8 @@ struct symTableItem{
 }
 
 char *reserved_table[] = {
-    "int", "if", "else",
-    "while", "for", "read", "write"
+    "integer", "if", "then", "else",
+    "begin", "end", "read", "write", "function"
 };
 
 int ScanOneToken(FILE *fp){
@@ -34,9 +41,16 @@ int ScanOneToken(FILE *fp){
 
 
 int main(void){
-    struct token_t token;
-    InitScanner();
-    FILE* f = fopen(argv[1], "r");   
+    FILE* f = fopen("source.txt", "r");
+    if (f == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    
+    int lineno = 0;
+    char *line = NULL;
+    size_t len = 0;
+    while()
+    
     while (ScanOneToken(f) != T_END)
         ; // 处理各个单词符号
     return 0;
